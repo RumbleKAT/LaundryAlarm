c05b0baf596f9306717bb093c34cc08baf014d79
RumbleKAT <reki318@naver.com>
2021-10-05 21:52:04 +0900

    Initial Commit

---
+++ LaundryAlarm/LaundryAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaundryAlarmApp.swift
+//  LaundryAlarm
+//
+//  Created by 송명진 on 2021/10/05.
+//
+
+import SwiftUI
+
+@main
+struct LaundryAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
